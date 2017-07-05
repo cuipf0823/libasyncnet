a27d938144dd685e30b0ce4d0c671d8e85fa8553
cuipf0823 <570094028@qq.com>
2017-07-06 01:00:12 +0800

    add file sinks

--- module/log/file_sinks.h
+++ module/log/file_sinks.h
@@ -1,14 +1,39 @@
 #ifndef ASYNCNET_LOG_FILE_SINKS_H
 #define ASYNCNET_LOG_FILE_SINKS_H
+##include <string>
+
 namespace asyncnet
 {
-
-
 namespace log
 {
+
+class Sinks
+{
+public:
+  Sinks();
+  virtual ~Sinks();
+  Sinks& operator=(const Sinks&) = delete;
+  Sinks(const Sinks&) = delete;
+
+  virtual void append(const std::string& buffer) = 0;
+  virtual void append(const char* buffer, int len) = 0;
+  virtual void flush() = 0;
+}
+
 class FileSinks : public Sinks
 {
+public:
+  FileSinks(std::string& path, size_t roll_size);
+  ~FileSinks();
+  FileSinks& operator=(const Sinks&) = delete;
+  FileSinks(const Sinks&) = delete;
 
+  virtual void append(const std::string& buffer);
+  virtual void append(const char* buffer, int len);
+  virtual void flush();
+private:
+  const std::string path_;
+  const size_t roll_size_;
 };
 
 }
--- module/log/tests/log_stream_test.cc
+++ module/log/tests/log_stream_test.cc
@@ -1,14 +1,32 @@
 #include <limits>
 #include <iostream>
+#include <string>
 #include "log_stream.h"
 
 int main(int argc, char *argv[])
 {
 	asyncnet::log::LogStream stream;
-	stream << std::numeric_limits<bool>::max();
+	stream << "bool min: " << std::numeric_limits<bool>::min();
+	stream << " bool max: " << std::numeric_limits<bool>::max();
+	stream << "\nshort min:" << std::numeric_limits<short>::min();
+	stream << " short max:" << std::numeric_limits<short>::max();
+	stream << "\nunsigned short min:" << std::numeric_limits<unsigned short>::min();
+	stream << " unsigned short max:" << std::numeric_limits<unsigned short>::max();
+	stream << "\nint min:" << std::numeric_limits<int>::min();
+	stream << " int max:" << std::numeric_limits<int>::max();
+	stream << "\nunsigned int min:" << std::numeric_limits<unsigned int>::min();
+	stream << " unsigned int max:" << std::numeric_limits<unsigned int>::max();
+	stream << "\nfloat min:" << std::numeric_limits<float>::min();
+	stream << " float max:" << std::numeric_limits<float>::max();
+	stream << "\ndouble min:" << std::numeric_limits<double>::min();
+	stream << " double max:" << std::numeric_limits<double>::max();
+	stream << "\n long long min:" << std::numeric_limits<long long>::min();
+	stream << " long long max:" << std::numeric_limits<long long>::max();
+	stream << "\n char:" << std::numeric_limits<char>::min();
+	stream << " char:" << std::numeric_limits<char>::max();
+	std::string str("string test!!");
+	//stream << "\n" << str;
+	stream << "\n end";
 	std::cout << stream.buffer() << std::endl;
-	stream << std::numeric_limits<int>::max();
-	std::cout << " " << stream.buffer() << std::endl;
-	std::cout << std::numeric_limits<int>::max() << std::endl;
 	return 0;
 }
